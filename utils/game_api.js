0a84e52d76d69de48293e7ae48713187a3acb83b
Carly Schmitting <cschmitting@gmail.com>
2019-04-30 15:10:07 +0800

    Update Files to Match with Sizhen

--- utils/game_api.js
+++ utils/game_api.js
@@ -1,29 +1,34 @@
-const createGame = function (page) {
-  // var value = wx.getStorageSync('token')
-  // console.log("create game test", value)
-  // if (value) {
-    console.log("create game check data", page);
-    wx.request({
-      url: `http://localhost:3000/api/v1/game`,
-      method: 'POST',
-      data: {
-        // "tokens": {
-        //   "token": value
-        // },
-        "game": {
-          "rounds": page.detail.value.rounds,
-          "find_partner_timer": page.detail.value.find_partner_timer,
-          "selfie_timer": page.detail.value.selfie_timer,
-          "question_timer": page.detail.value.question_timer
-        },
-      },
-      success: res => {
-        wx.redirectTo({
-          url: '../listing/listing',
-        })
-      }
-    })
-  // }
-}
+// import { createGame } from '../../utils/game_api.js';
+// createGameSubmit: (e) => {
+//   createGame(e);
+// },
 
-export { createGame };
+// const createGame = function (page) {
+//   // var value = wx.getStorageSync('token')
+//   // console.log("create game test", value)
+//   // if (value) {
+//     console.log("create game check data", page);
+//     wx.request({
+//       url: `http://localhost:3000/api/v1/game`,
+//       method: 'POST',
+//       data: {
+//         // "tokens": {
+//         //   "token": value
+//         // },
+//         "game": {
+//           "rounds": page.detail.value.rounds,
+//           "find_partner_timer": page.detail.value.find_partner_timer,
+//           "selfie_timer": page.detail.value.selfie_timer,
+//           "question_timer": page.detail.value.question_timer
+//         },
+//       },
+//       success: res => {
+//         wx.redirectTo({
+//           url: '../listing/listing',
+//         })
+//       }
+//     })
+//   // }
+// }
+
+// export { createGame };
