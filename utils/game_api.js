2739695dc3b97a426e6539bcce320cf50df90ba8
Carly Schmitting <cschmitting@gmail.com>
2019-05-02 07:15:06 +0800

    JS: update creation page

--- utils/game_api.js
+++ utils/game_api.js
@@ -1,6 +1,11 @@
+const app = getApp();
+let globalData = app.globalData;
+let g = globalData || {};
+let gameTime = g.gameTime || [];
+let gameTimeIndices = g.gameTimeIndices || [];
+
 const createGame = function (page) {
   var value = wx.getStorageSync('token')
-  console.log("create game test", value)
   if (value) {
     console.log("create game check data", page);
     wx.request({
@@ -11,7 +16,7 @@ const createGame = function (page) {
           "token": value
         },
         "game": {
-          "rounds": page.detail.value.rounds,
+          "rounds": page.detail.value.round_number,
           "find_partner_timer": page.detail.value.find_partner_timer,
           "selfie_timer": page.detail.value.selfie_timer,
           "question_timer": page.detail.value.question_timer
@@ -26,19 +31,23 @@ const createGame = function (page) {
   }
 }
 
-const verifyInteger = function () {
-  var userInput;
-  var errorMessage;
+const setTime = function (e, f, gameTimeIndex) {
+  let defineTimeIndex = gameTimeIndex + "Time"
+  
+  let arrayOfMinutes = gameTime.minute_possibilities
+  let minute = Number.parseInt(arrayOfMinutes[e.detail.value[0]], 10);
 
-  userInput = document.getElementById("numb").value;
-  console.log("starting to verify");
-  console.log("User Input", userInput);
+  let arrayOfSeconds = gameTime.second_possibilities
+  let second = Number.parseInt(arrayOfSeconds[e.detail.value[1]], 10);
 
-  if (isNaN(userInput) || userInput < 1) {
-    errorMessage = "Please enter a valid number";
-    console.log(errorMessage);
-  }
-  document.getElementById("demo").innerHTML = errorMessage;
+  app.globalData.gameTimeIndices[defineTimeIndex].minutes = minute
+  app.globalData.gameTimeIndices[defineTimeIndex].seconds = second
+
+  const gameTimeIndices = app.globalData.gameTimeIndices
+
+  f.setData({
+    gameTimeIndices
+  })
 }
 
-export { createGame };
+export { createGame, setTime };
