7578d665b20cdd161f3153077a9b48cbbe595786
Sizhen Rao <sizhenrao@icloud.com>
2019-05-02 15:21:15 +0800

    working on question page

--- pages/question/question.js
+++ pages/question/question.js
@@ -1,66 +1,22 @@
 // pages/question/question.js
 Page({
 
-  /**
-   * Page initial data
-   */
-  data: {
-
+  goToQuestion: function () {
+    wx.navigateTo({
+      url: '/pages/question/question'
+    })
   },
 
-  /**
-   * Lifecycle function--Called when page load
-   */
+  // top bar styling
   onLoad: function (options) {
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: '#DFFBFE',
+    })
 
+    wx.setNavigationBarTitle({
+      title: 'Question time!',
+    })
   },
 
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
-  }
 })
