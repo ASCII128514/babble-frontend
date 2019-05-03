eef0233ca34c6c706dcb3463defc867c2bbbfddc
Carly Schmitting <cschmitting@gmail.com>
2019-05-03 12:54:01 +0800

    Update play_game_api.js

---
+++ utils/play_game_api.js
@@ -0,0 +1,20 @@
+// const createGame = function (objectOfSeconds, page) {
+//   wx.request({
+//     url: ``,
+//     method: '',
+//     data: {
+//       "tokens": {
+//         "token": value
+//       },
+//       "game": {
+//         "round_number": numberOfRounds,
+//         "find_partner_timer": objectOfSeconds.find_partner_timer,
+//         "selfie_timer": objectOfSeconds.selfie_timer,
+//         "question_timer": objectOfSeconds.question_timer
+//       },
+//     },
+//   },
+//   success: res => {
+//     console.log(res.data);
+//   }
+// })
