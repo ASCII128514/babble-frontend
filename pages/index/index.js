6afcfd98e3e20c47f3021a4a10cafde929723f55
Carly Schmitting <cschmitting@gmail.com>
2019-05-03 13:12:05 +0800

    JS/WS: updated how to read QR code and websocket message

--- pages/index/index.js
+++ pages/index/index.js
@@ -27,7 +27,7 @@ Page({
       } else {
         scene = query.scene
       }
-      console.log(scene);
+      console.log("?", scene);
       getApp().globalData.qrCodeData = scene
 
       if (scene !== null) {
@@ -88,6 +88,15 @@ Page({
                 // console.log(res)
                 if (value.type != 'ping' && value.type != 'welcome') {
                   console.log('hahaha', value)
+
+                  switch (value.message.type) {
+                    case "users":
+                      getApp().globalData.playerList = value.message.playerss
+                      break;
+                    case "pairs":
+                      getApp().globalData.playerPairs = value.message.pairs
+                      break;
+                  }
                 }
 
               })
