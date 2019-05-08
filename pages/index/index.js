c011b45abd88d35e273ed19559a6505711a7ad84
Sizhen Rao <sizhenrao@icloud.com>
2019-05-08 20:55:36 +0800

    UI improved

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,7 +13,7 @@ Page({
   onLoad: function () {
     wx.setNavigationBarColor({
       frontColor: '#ffffff',
-      backgroundColor: '#A7C3EC',
+      backgroundColor: '#5a97f1',
     })
     wx.setNavigationBarTitle({
       title: 'Welcome',
