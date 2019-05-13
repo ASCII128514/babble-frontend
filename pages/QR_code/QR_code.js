e26bfb1650f6a7565749741563a8ee1b8de266ea
Paul Lu <luhanfeng1106@sina.com>
2019-05-13 18:20:09 +0800

    finish qrcode

--- pages/QR_code/QR_code.js
+++ pages/QR_code/QR_code.js
@@ -1,40 +1,28 @@
 // pages/QR_code/QR_code.js
-let app = getApp()
+const app = getApp();
 
 Page({
-  buttonClicked: function () {
-    var page = this
+  buttonClicked() {
     wx.reLaunch({
-      url: `/pages/login/login?scene=${this.data.roomId}`
-    })
+      url: `/pages/login/login?scene=${this.data.roomId}`,
+    });
   },
 
-  /**
-   * Page initial data
-   */
-  data: {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad: function (options) {
+  onLoad() {
     this.setData({
       qrCodeUrl: app.globalData.qrCodeUrl,
-      roomId: app.globalData.roomId
-    })
+      roomId: app.globalData.roomId,
+    });
     wx.setStorageSync('room', app.globalData.roomId);
-    app.globalData.roomId = null
+    app.globalData.roomId = null;
 
     wx.setNavigationBarColor({
       frontColor: '#ffffff',
       backgroundColor: '#5a97f1',
-    })
+    });
 
     wx.setNavigationBarTitle({
       title: 'Share QR Code',
-    })
-
-  }
-})
+    });
+  },
+});
