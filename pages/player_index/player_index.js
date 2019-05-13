06a177a3eca2e931e714a1d7461b8346a7e7d2b5
Paul Lu <luhanfeng1106@sina.com>
2019-05-13 18:18:59 +0800

    finish player index

--- pages/player_index/player_index.js
+++ pages/player_index/player_index.js
@@ -1,64 +1,54 @@
 // pages/player_index/player_index.js
-const app = getApp()
+import { sendNameToBackend } from '../../utils/create_game_api';
 
-import { sendNameToBackend } from '../../utils/create_game_api.js';
+const app = getApp();
 
 Page({
   data: {
     modalHidden: true,
   },
 
-  nameSubmit: function (e) {
-    var patt = new RegExp(/^(.{1,10}\w)$/);
-    var verification = patt.test(e.detail.value.name);
-    console.log("verified?", verification);
+  nameSubmit(e) {
+    const patt = new RegExp(/^(.{1,10}\w)$/);
+    const verification = patt.test(e.detail.value.name);
+    console.log('verified?', verification);
 
-    if (verification == false) {
-      this.setData ({
-        modalHidden: false
-      })
-
-
-    } else if (verification == true) {
+    if (verification === false) {
+      this.setData({
+        modalHidden: false,
+      });
+    } else if (verification === true) {
       sendNameToBackend(e);
       wx.navigateTo({
-        url: '/pages/take_selfie/take_selfie'
-      })
-    }    
+        url: '/pages/take_selfie/take_selfie',
+      });
+    }
   },
 
-  modalCancel: function () {
+  modalCancel() {
     this.setData({
-      modalHidden: true
-    })
-
+      modalHidden: true,
+    });
   },
 
-  modalConfirm: function () {
+  modalConfirm() {
     this.setData({
-      modalHidden: true
-    })
-
+      modalHidden: true,
+    });
   },
 
-  toInstruction: function () {
-    wx.navigateTo({
-      url: '/pages/instruction_p/instruction_p'
-    })
-  },
-
-  onLoad: function (options) {
+  onLoad() {
     wx.setNavigationBarColor({
       frontColor: '#ffffff',
       backgroundColor: '#5a97f1',
-    })
+    });
 
     wx.setNavigationBarTitle({
       title: 'Welcome',
-    })
+    });
     this.setData({
-      name: app.globalData.currentUser.name
-    })    
+      name: app.globalData.currentUser.name,
+    });
   },
 
-})
+});
