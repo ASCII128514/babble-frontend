b7620ed2bfc3a72762ef971d3590bc2b7ed8c944
Paul Lu <luhanfeng1106@sina.com>
2019-05-13 18:42:54 +0800

    finish everyline of refactor

--- pages/timer_question/timer_question.js
+++ pages/timer_question/timer_question.js
@@ -1,59 +1,55 @@
 // pages/timer_question/timer_question.js
-import { createGame, setTime, convertArrayToSeconds } from '../../utils/create_game_api.js';
+import { createGame, convertArrayToSeconds } from '../../utils/create_game_api';
 
 Page({
 
-  buttonClicked: function () {
-    let objectOfSeconds = convertArrayToSeconds();
+  buttonClicked() {
+    const objectOfSeconds = convertArrayToSeconds();
     createGame(objectOfSeconds, this);
   },
-  /**
-   * Page initial data
-   */
+
   data: {
     gameTime: {},
     gameTimeIndices: {},
   },
 
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad: function (options) {
+  onLoad() {
     wx.setNavigationBarColor({
       frontColor: '#ffffff',
       backgroundColor: '#5a97f1',
-    })
+    });
 
     wx.setNavigationBarTitle({
       title: 'Game setting',
-    })
+    });
     // Copy arrays from globalData to data
-    const gameTime = getApp().globalData.gameTime
-    this.setData({ gameTime })
+    const { gameTime } = getApp().globalData;
+    const { gameTimeIndices } = getApp().globalData;
 
-    let gameTimeIndices = getApp().globalData.gameTimeIndices
-    this.setData({ gameTimeIndices,
+    this.setData({
+      gameTime,
+      gameTimeIndices,
       minute: gameTimeIndices.questionTime.minutes,
-      second: gameTimeIndices.questionTime.seconds })
+      second: gameTimeIndices.questionTime.seconds,
+    });
   },
 
-  questionTimeAmount: function (res) {
-
-    getApp().globalData.gameTimeIndices.questionTime.minutes = `${res.detail.value[0]}`
-    var s
+  questionTimeAmount(res) {
+    getApp().globalData.gameTimeIndices.questionTime.minutes = `${res.detail.value[0]}`;
+    let s;
     if (res.detail.value[1] === 0) {
-      s = '00'
+      s = '00';
     } else {
-      s = `${res.detail.value[1] * 15}`
+      s = `${res.detail.value[1] * 15}`;
     }
-    getApp().globalData.gameTimeIndices.questionTime.seconds = s
-    let gameTimeIndices = getApp().globalData.gameTimeIndices
-    console.log(gameTimeIndices)
-    console.log(getApp().globalData)
+    getApp().globalData.gameTimeIndices.questionTime.seconds = s;
+    const { gameTimeIndices } = getApp().globalData;
+    console.log(gameTimeIndices);
+    console.log(getApp().globalData);
     this.setData({
       gameTimeIndices,
       minute: gameTimeIndices.questionTime.minutes,
-      second: gameTimeIndices.questionTime.seconds
-    })
-  }
-})
+      second: gameTimeIndices.questionTime.seconds,
+    });
+  },
+});
