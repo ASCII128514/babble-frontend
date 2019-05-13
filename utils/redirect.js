086d43383539f46e04102c58d41e118e875da976
Paul Lu <luhanfeng1106@sina.com>
2019-05-13 23:18:42 +0800

    refactor redirect

--- utils/redirect.js
+++ utils/redirect.js
@@ -1,128 +1,110 @@
-const switching = function (query, page) {
+import listenSocket from './websocket';
+
+const switching = (query, page) => {
   // check whether the user already have a game stored in his storage before everything else
   // make request to the api to check whether the game is ended
   // set the scene to that room if it is not expired
   // clear it if the room is expired.
   let scene = null;
-  var storageRoom = wx.getStorageSync('room')
+  const storageRoom = wx.getStorageSync('room');
 
   if (storageRoom) {
     // make api call to check whether the room is expired
-    scene = storageRoom
+    scene = storageRoom;
     page.setData({
-      modalHidden: false
-    })
-    console.log("this is running")
+      modalHidden: false,
+    });
   }
-  console.log(scene)
 
-  // console.log(typeof query.scene);
-  console.log(query)
+
   if (typeof query.scene === 'undefined' && scene === null) {
     wx.reLaunch({
       url: '/pages/index/index',
-    })
-    return
+    });
+    return;
   }
-  if (typeof query.scene === "string") {
-    console.log("query:", query);
+  if (typeof query.scene === 'string') {
+    console.log('query:', query);
     scene = decodeURIComponent(query.scene);
   }
-  console.log("scene", scene);
+  console.log('scene', scene);
   getApp().globalData.qrCodeData = scene;
-  console.log(scene !== "undefined");
+  console.log(scene !== 'undefined');
   console.log(scene !== null);
-  console.log("in redirect")
-  console.log(scene)
-  if (scene !== null && scene !== "undefined") {
+  console.log('in redirect');
+  console.log(scene);
+  if (scene !== null && scene !== 'undefined') {
     wx.setStorageSync('room', scene);
-    console.log('set storage')
+    console.log('set storage');
     wx.request({
       url: `https://babble.wogengapp.cn/api/v1/game/${scene}`,
-      method: "get",
-      success: res => {
-        console.log("res data:", res.data);
-        if (res.data.game.status !== "end") {
-          console.log(res)
+      method: 'get',
+      success: (res) => {
+        console.log('res data:', res.data);
+        if (res.data.game.status !== 'end') {
+          console.log(res);
           getApp().globalData.players = res.data.players;
           getApp().globalData.numberOfRounds = res.data.game.round_number;
-          var x = res.data.game.find_partner_timer % 60
+          let x = res.data.game.find_partner_timer % 60;
           if (x === 0) {
-            x = '00'
+            x = '00';
           } else {
-            x = `${x}`
+            x = `${x}`;
           }
-          getApp().globalData.gameTimeIndices.partnerTime.seconds = x
-          var m = Number.parseInt(res.data.game.find_partner_timer / 60)
-          getApp().globalData.gameTimeIndices.partnerTime.minutes = m
-          console.log('mintue', m)
-          var y = res.data.game.question_timer % 60
+          getApp().globalData.gameTimeIndices.partnerTime.seconds = x;
+          const m = Number.parseInt(res.data.game.find_partner_timer / 60, 10);
+          getApp().globalData.gameTimeIndices.partnerTime.minutes = m;
+          console.log('mintue', m);
+          let y = res.data.game.question_timer % 60;
           if (y === 0) {
-            y = '00'
+            y = '00';
           } else {
-            y = `${y}`
+            y = `${y}`;
           }
-          getApp().globalData.gameTimeIndices.questionTime.seconds = y
-          var j = Number.parseInt(res.data.game.question_timer / 60)
-          getApp().globalData.gameTimeIndices.questionTime.minutes = j
-          console.log('mintue', getApp().globalData.gameTimeIndices.questionTime.minutes)
+          getApp().globalData.gameTimeIndices.questionTime.seconds = y;
+          const j = Number.parseInt(res.data.game.question_timer / 60, 10);
+          getApp().globalData.gameTimeIndices.questionTime.minutes = j;
+          console.log('mintue', getApp().globalData.gameTimeIndices.questionTime.minutes);
 
           const response = res;
           wx.connectSocket({
-            url: "wss://babble.wogengapp.cn/cable",
+            url: 'wss://babble.wogengapp.cn/cable',
             header: {
-              "content-type": "application/json",
+              'content-type': 'application/json',
             },
           });
-          wx.onSocketOpen(function (res) {
+          wx.onSocketOpen(() => {
             const id = JSON.stringify({
-              channel: "GameChannel",
+              channel: 'GameChannel',
               room: response.data.game.id,
-              token: wx.getStorageSync("token"),
+              token: wx.getStorageSync('token'),
             });
             wx.sendSocketMessage({
               data: JSON.stringify({
-                command: "subscribe",
+                command: 'subscribe',
                 identifier: id,
                 // data: data
               }),
             });
           });
-          wx.onSocketMessage(function (res) {
-            const value = JSON.parse(res.data);
-
-            if (
-              value.type != "ping" &&
-              value.type != "welcome" &&
-              value.type != "confirm_subscription"
-            ) {
-              if (value.message.type == "users") {
-                console.log("players:", value.message.players);
-                getApp().globalData.playerList = value.message.players;
-              } else if (value.message.type == "pairs") {
-                console.log("save that pair shit");
-              }
-            }
-          });
+          listenSocket(this);
           wx.reLaunch({
-            url: "/pages/player_home_page/player_home_page",
+            url: '/pages/player_home_page/player_home_page',
           });
         } else {
           // clear the phone's storage
           wx.setStorageSync('room', null);
           wx.reLaunch({
             url: '/pages/index/index',
-          })
+          });
         }
-      }
+      },
     });
   } else {
     wx.reLaunch({
       url: '/pages/index/index',
-    })
+    });
   }
 };
 
-export {
-  switching
-}
+export default switching;
