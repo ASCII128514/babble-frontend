7dc81b03e7ff61bf04db88957d621d9881015626
Paul Lu <luhanfeng1106@sina.com>
2019-05-13 17:58:22 +0800

    finish instruction

--- pages/instruction/instruction.js
+++ pages/instruction/instruction.js
@@ -1,10 +1,10 @@
 // pages/instruction/instruction.js
 Page({
 
-  buttonClicked: function () {
+  buttonClicked() {
     wx.navigateTo({
-      url: '/pages/set_rounds/set_rounds'
-    })
+      url: '/pages/set_rounds/set_rounds',
+    });
   },
 
   /**
@@ -17,65 +17,13 @@ Page({
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function (options) {
-    
-      wx.setNavigationBarColor({
-        frontColor: '#ffffff',
-        backgroundColor: '#5a97f1',
-      })
-
-      wx.setNavigationBarTitle({
-        title: 'How to play',
-      })
-
+  onLoad() {
+    wx.setNavigationBarColor({
+      frontColor: '#ffffff',
+      backgroundColor: '#5a97f1',
+    });
+    wx.setNavigationBarTitle({
+      title: 'How to play',
+    });
   },
-
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+});
