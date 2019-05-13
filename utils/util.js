138c5dd35d0c684bcb612fabaad4fe931f044bd5
Paul Lu <luhanfeng1106@sina.com>
2019-05-13 23:04:00 +0800

    finish util

--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,18 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+const formatNumber = (n) => {
+  const x = n.toString();
+  return x[1] ? x : `0${x}`;
+};
+const formatTime = (date) => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-}
-
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
-}
+  return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`;
+};
 
 module.exports = {
-  formatTime: formatTime
-}
+  formatTime,
+};
