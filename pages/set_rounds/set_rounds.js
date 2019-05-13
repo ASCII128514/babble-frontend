0e6992a9a3824ffd005e5f5484f67ef23a5430fd
Paul Lu <luhanfeng1106@sina.com>
2019-05-13 18:21:35 +0800

    finish set_rounds

--- pages/set_rounds/set_rounds.js
+++ pages/set_rounds/set_rounds.js
@@ -1,38 +1,26 @@
 // pages/set_rounds/set_rounds.js
 Page({
 
-  buttonClicked: function () {
+  buttonClicked() {
     wx.navigateTo({
-      url: '/pages/timer_find_partner/timer_find_partner'
-    })
+      url: '/pages/timer_find_partner/timer_find_partner',
+    });
   },
 
-
-  /**
-   * Page initial data
-   */
-  data: {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad: function (options) {
-
+  onLoad() {
     wx.setNavigationBarColor({
       frontColor: '#ffffff',
       backgroundColor: '#5a97f1',
-    })
+    });
 
     wx.setNavigationBarTitle({
       title: 'Game setting',
-    })
+    });
   },
 
-  roundsSlider: function(res) {
-    console.log(res.detail.value)
-    getApp().globalData.numberOfRounds = res.detail.value
-    console.log(getApp().globalData.numberOfRounds )
-  }
-})
+  roundsSlider(res) {
+    console.log(res.detail.value);
+    getApp().globalData.numberOfRounds = res.detail.value;
+    console.log(getApp().globalData.numberOfRounds);
+  },
+});
