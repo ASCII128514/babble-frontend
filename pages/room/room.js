2176ef9da069de09850c6cb921175ec68fb188a2
Paul Lu <luhanfeng1106@sina.com>
2019-05-13 17:21:36 +0800

    modified the listen websocket in room

--- pages/room/room.js
+++ pages/room/room.js
@@ -1,74 +1,43 @@
+import listenSocket from '../../utils/websocket';
+
 // pages/room/room.js
 Page({
 
-
   data: {
-    numOfPeople: 0
+    numOfPeople: 0,
   },
 
-  onLoad: function (options) {
-
+  onLoad() {
     // top bar styling
     wx.setNavigationBarColor({
       frontColor: '#ffffff',
       backgroundColor: '#5a97f1',
-    })
+    });
 
     wx.setNavigationBarTitle({
       title: 'Waiting room',
-    })
+    });
     // top bar styling
 
-    const playerList = getApp().globalData.playerList
-    const page = this
+    const { playerList } = getApp().globalData;
+    const page = this;
     page.setData({
-      playerList
-    })
-    wx.onSocketMessage(function (res) {
-      const value = JSON.parse(res.data)
-      console.log('in room')
-      if (value.type != 'ping' && value.type != 'welcome' && value.type != 'confirm_subscription') {
-        if (value.message.type == "users") {
-          console.log("players:", value.message.players);
-          page.setData({
-            playerList: value.message.players
-          })
-          page.setData({
-            numOfPeople: value.message.players.length
-          })
-
-
-        } else if (value.message.type == "pair") {
-          getApp().globalData.pair = value.message.pairs[wx.getStorageSync('token')]
-          getApp().globalData.currentGameRound = value.message.round
-          console.log(value.message.pairs)
-          console.log(getApp().globalData.pair)
-          console.log("question", getApp().globalData.pair.question)
-          wx.redirectTo({
-            url: '/pages/find_partner/find_partner'
-          })
-        } else if (value.message.type == 'finish') {
-          wx.closeSocket()
-          wx.setStorageSync('room', null);
-          wx.reLaunch({
-            url: '/pages/finished_game/finished_game',
-          })
-        }
-      }
-    })
-    wx.onSocketClose(function (res) {
-      console.log('WebSocket 已关闭！')
-    })
+      playerList,
+    });
+    listenSocket(page);
+    wx.onSocketClose(() => {
+      console.log('WebSocket 已关闭！');
+    });
   },
-  
-  getPair: function () {
+
+  getPair() {
     wx.request({
-      url: `https://babble.wogengapp.cn/api/v1/game/${getApp().globalData.qrCodeData}/pair?round=${getApp().globalData.currentGameRound + 1}&token=${wx.getStorageSync('token')}`
-    })
+      url: `https://babble.wogengapp.cn/api/v1/game/${getApp().globalData.qrCodeData}/pair?round=${getApp().globalData.currentGameRound + 1}&token=${wx.getStorageSync('token')}`,
+    });
   },
-  onReady: function () {
+  onReady() {
     wx.request({
       url: `https://babble.wogengapp.cn/api/v1/game/${getApp().globalData.qrCodeData}/display`,
-    })
-  }
-})
+    });
+  },
+});
